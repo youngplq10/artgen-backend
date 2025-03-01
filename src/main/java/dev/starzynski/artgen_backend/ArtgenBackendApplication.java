d8329fb745ae0ea0d66707f5321091a1345cfe07
youngplq10 <patryk.plq@o2.pl>
2025-03-01 18:45:07 +0100

    Initial commit

---
+++ src/main/java/dev/starzynski/artgen_backend/ArtgenBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.starzynski.artgen_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtgenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtgenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/starzynski/artgen_backend/ArtgenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starzynski.artgen_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtgenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
