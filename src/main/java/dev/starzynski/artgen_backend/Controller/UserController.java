3f721f6131ec9e7a56163677b86e89be96404ffd
youngplq10 <patryk.plq@o2.pl>
2025-03-02 13:59:00 +0100

    Created login and register services

--- src/main/java/dev/starzynski/artgen_backend/Controller/UserController.java
+++ src/main/java/dev/starzynski/artgen_backend/Controller/UserController.java
@@ -1,11 +1,28 @@
 package dev.starzynski.artgen_backend.Controller;
 
+import dev.starzynski.artgen_backend.Model.User;
 import dev.starzynski.artgen_backend.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @PostMapping("/public/user/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/public/user/login")
+    public ResponseEntity<?> loginUser(@RequestBody User user) {
+        return userService.loginUser(user);
+    }
 }
--- src/main/java/dev/starzynski/artgen_backend/Service/UserService.java
+++ src/main/java/dev/starzynski/artgen_backend/Service/UserService.java
@@ -1,11 +1,66 @@
 package dev.starzynski.artgen_backend.Service;
 
+import dev.starzynski.artgen_backend.Model.User;
 import dev.starzynski.artgen_backend.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    private AuthenticationManager authManager;
+
+    public ResponseEntity<?> registerUser(User user) {
+        try {
+            Boolean isExisting = userRepository.findByUsername(user.getUsername()).isPresent();
+
+            if (isExisting) {
+                return ResponseEntity
+                        .status(HttpStatus.CONFLICT)
+                        .body(Collections.singletonMap("message", "User already exists."));
+            }
+
+            userRepository.insert(user);
+
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(Collections.singletonMap("jwt", jwtService.generateToken(user.getUsername())));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ResponseEntity<?> loginUser(User user) {
+        try {
+            Authentication authentication = authManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+            );
+
+            if(authentication.isAuthenticated()){
+                return ResponseEntity
+                        .status(HttpStatus.OK)
+                        .body(Collections.singletonMap("jwt", jwtService.generateToken(user.getUsername())));
+            }
+        } catch (AuthenticationException e) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(Collections.singletonMap("message", "User doesn't exist."));
+        }
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Collections.singletonMap("message", "Server error in login service."));
+    }
 }
